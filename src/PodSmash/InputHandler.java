85abba1d2bd115ee1223740496c326c3107fd609
Dylan <dylanbaker7@gmail.com>
2018-10-22 11:32:39 -0400

    Readded

--- src/PodSmash/InputHandler.java
+++ src/PodSmash/InputHandler.java
@@ -1,5 +1,91 @@
 package PodSmash;
 
-public class InputHandler {
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
+public class InputHandler implements KeyListener, FocusListener, MouseListener, MouseMotionListener{
+
+	public boolean[] keys = new boolean[65536];
+	
+	public int mouseX = 0, mouseY = 0;
+	
+	public boolean isClicked = false;
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		mouseX = e.getX();
+		mouseY = e.getY();
+	}
+
+	@Override
+	public void focusGained(FocusEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		for(int i = 0; i < keys.length; i++)
+			keys[i] = false;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		if(keyCode>0 && keyCode<keys.length)
+			keys[keyCode] = true;
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		if(keyCode>0 && keyCode<keys.length)
+			keys[keyCode] = false;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		isClicked = true;
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		isClicked = false;
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- src/PodSmash/Vector.java
+++ src/PodSmash/Vector.java
@@ -2,4 +2,26 @@ package PodSmash;
 
 public class Vector {
 
+	public double dx,dy;
+	
+	public Vector(double dx, double dy){
+		this.dx = dx;
+		this.dy = dy;
+	}
+	
+	public double getMag(){
+		return Math.sqrt((dx*dx) + (dy*dy));
+	}
+	
+	public void scale(double factor){
+		dx*=factor;
+		dy*=factor;
+	}
+	
+	public double getCross(Vector v){
+		return (v.dx*dy) - (v.dy*dx);
+	}
+	
+	
+	
 }
